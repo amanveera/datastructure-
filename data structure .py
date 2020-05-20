3c8c420b7c33eb451c0908984407d60889744793
amanveera <65596130+amanveera@users.noreply.github.com>
2020-05-20 22:12:49 +0530

    data structure
    data structure types

---
+++ data structure .py
@@ -0,0 +1,18 @@
+#lists
+List1 = ['mom','dad','me','grandpa','grandma']
+List2 = ['40','44','20','67','65']
+
+List1.append('maternal grandparents')
+print(List1)
+
+#tuple
+Tuple1 = ("aman","pooja","manoj","keerthi")
+print(Tuple1[3])
+
+
+#dictionary
+myDict = {"sub1":"science","sub2":"maths","sub3":"social studies","sub4":"english"}
+del myDict["sub2"]
+print(myDict)
+
+
